fbab5bcaef44cfdbc769870dd8d20d30ecabf4a4
taiinteli <99188288+taiinteli@users.noreply.github.com>
2024-10-02 09:45:51 -0300

    Update blog_posts.py

--- app/models/blog_posts.py
+++ app/models/blog_posts.py
@@ -1,10 +1,15 @@
+    
 class BlogPost:
-    def __init__(self, id, title, content):
-        self.id = id
-        self.title = title
-        self.content = content
+    def __init__(id, self, usuario, review, serie_filme, estrelas):
+        self.id = '1'
+        self.usuario = 'nome_usuario'
+        self.review = 'este filme foi supimpa'
+        self.serie_filme = 'pokemon a vingança de mewteo'
+        self.estrelas = '5'
+
     def __str__(self) -> str:
-        return f'{self.id} - {self.title} - {self.content}'
+        return f'{self.id} - {self.usuario} - {self.review} - {self.serie_filme} - {self.estrelas}'
     
     def toJson(self):
-        return {'id': self.id, 'title': self.title, 'content': self.content}
+        return {'id': self.id, 'usuario': self.usuario, 'review': self.review, 'serie_filme': self.serie.filme, 'estrelas': self.estrelas}
+    
